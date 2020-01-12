c55409b3860232845abd2878c2deb2462a31aa80
Milena Mankowska <milenamankowska@gmail.com>
2020-01-12 18:06:07 +0100

    added editing of todo

--- src/main/java/com/myla/todolist/ToDoEntry.java
+++ src/main/java/com/myla/todolist/ToDoEntry.java
@@ -19,4 +19,6 @@ public class ToDoEntry {
 
     private String name;
 
+    private boolean editInProgress;
+
 }
--- src/main/java/com/myla/todolist/ToDoListController.java
+++ src/main/java/com/myla/todolist/ToDoListController.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -22,6 +23,22 @@ public class ToDoListController {
 
     @PostMapping("/add")
     public String add(ToDoEntry toDoEntry) {
+        toDoListRepository.save(toDoEntry);
+        return "redirect:/show";
+    }
+
+    @PostMapping("/edit/{id}")
+    public String edit(ToDoEntry toDoEntry, @PathVariable(name="id") Long id){
+        toDoEntry.setId(id);
+        toDoEntry.setEditInProgress(false);
+        toDoListRepository.save(toDoEntry);
+        return "redirect:/show";
+    }
+
+    @PostMapping("/setToEdit/{id}")
+    public String setToEdit(ToDoEntry toDoEntry, @PathVariable(name="id") Long id){
+        toDoEntry.setId(id);
+        toDoEntry.setEditInProgress(true);
         toDoListRepository.save(toDoEntry);
         return "redirect:/show";
     }
